199d7ed5e49bb07fa7cb288ec095288845323d2b
Edward Roddriguez <edwardR36@hotmail.com>
2019-12-05 14:44:46 -0500

    part 2 of day 1 complete

--- day-1/day1.js
+++ day-1/day1.js
@@ -1,13 +1,32 @@
 const fs = require('fs');
 var data = fs.readFileSync('input.txt', 'utf-8').split('\n');
-let totalFuelRequired = 0;
+let fuelRequired = 0;
+let actualFuelRequired = 0;
 
 function fuelCalculator(mass) {
   return Math.floor(mass / 3) - 2;
 }
 
+//(PART 1) calculate total fuel needed
 data.forEach(function(module) {
-  totalFuelRequired += fuelCalculator(module);
+  fuelRequired += fuelCalculator(module);
 });
 
-console.log('total sum = ' + totalFuelRequired);
+console.log('total sum = ' + fuelRequired);
+
+//(PART 2)
+function realFuelCalculator(mass) {
+  var realModuleFuelRequirement = 0;
+  while (mass > 5) {
+    mass = fuelCalculator(mass);
+    realModuleFuelRequirement += mass;
+  }
+  return realModuleFuelRequirement;
+}
+
+//Calculate total sum of the actual fuel required for each module
+data.forEach(function(module) {
+  actualFuelRequired += realFuelCalculator(module);
+});
+
+console.log('actual sum = ' + actualFuelRequired);
